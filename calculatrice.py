2367f53b7a3a2bd41c6eb6689ebd3c62d5e9fb6d
Anais <anais.git15gmail.com>
2021-04-27 15:22:45 +0200

    Division et Soustraction

--- calculatrice.py
+++ calculatrice.py
@@ -31,7 +31,7 @@ def division(number):
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
-            list_numbers.append(int(number)) # do_something
+            list_numbers.append(int(number)) #conversion en entier
         number = ask_user("Saisir un chiffre à diviser ou clicker sur '=' ")
     for list_number, index in zip(list_numbers,list(range(len(list_numbers)))): # refactoriser
         if index == 0:
@@ -43,10 +43,10 @@ def division(number):
 
 def soustraction(number):
     list_numbers = []
-    while number.isdigit: # do_something
+    while number.isdigit(): # do_something
         if number.isdigit():
-            list_numbers.append(number) # do_something
-        number = ask_user("Saisir un chiffre à additionner ou clicker sur '=' ")
+            list_numbers.append(int(number)) #conversion en int
+        number = ask_user("Saisir un chiffre à soustraire ou clicker sur '=' ")
     i = 0
     for list_number in list_numbers:
         if i == 0:
